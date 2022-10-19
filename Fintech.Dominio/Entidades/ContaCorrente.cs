1bda19ae9e8a8b6bfe5d943fccbea5ab5e58d048
Henrique Ferraresi <Henrique@1ngc5k.onmicrosoft.com>
2022-10-18 21:57:34 -0300

    Modelagem Fintech

---
+++ Fintech.Dominio/Entidades/ContaCorrente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fintech.Dominio.Entidades
+{
+    public class ContaCorrente
+    {
+        public int Id { get; set; }
+        public int Numero { get; set; }
+        public string DigitoVerificador { get; set; }
+        public decimal Saldo { get; set; }
+        public Agencia Agencia { get; set; }
+        public Cliente Cliente { get; set; }
+
+        public void EfetuarOperacao(decimal valor, TipoOperacao tipoOperacao)
+        {
+            switch (tipoOperacao)
+            {
+                case TipoOperacao.Deposito:
+                    Saldo += valor;
+                    break;
+                case TipoOperacao.Saque:
+                    Saldo -= valor;
+                    break;
+                
+            }
+        }
+    }
+}
